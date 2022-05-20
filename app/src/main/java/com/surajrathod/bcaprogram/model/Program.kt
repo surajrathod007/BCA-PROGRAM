8115ed14de35de19d169e508c9470209124dda7e
Suraj <surajsinhrathod75@gmail.com>
2022-05-20 21:15:08 +0530

    Initial commit

---
+++ app/src/main/java/com/surajrathod/bcaprogram/model/Program.kt
@@ -0,0 +1,10 @@
+package com.surajrathod.bcaprogram.model
+
+data class Program (
+    val id : Int,
+    val title : String,
+    val content : String,
+    val sem : String,
+    val sub : String,
+    val unit : String
+)
