8f806936d91125305e62829bfaae042fc6f8ff34
Suraj <surajsinhrathod75@gmail.com>
2022-05-21 08:54:42 +0530

    Bottom Navigation Added

--- app/src/main/java/com/surajrathod/bcaprogram/ui/MainActivity.kt
+++ app/src/main/java/com/surajrathod/bcaprogram/ui/MainActivity.kt
@@ -2,11 +2,17 @@ package com.surajrathod.bcaprogram.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.surajrathod.bcaprogram.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        bottomNavigationView.setupWithNavController(findNavController(R.id.fragmentContainerView))
     }
 }
