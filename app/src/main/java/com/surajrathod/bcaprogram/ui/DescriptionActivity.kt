e517f31ee9f51d3974036d1207be9412bc75d8fd
Manshal <manshalkhatri51@gmail.com>
2022-05-23 19:50:31 +0530

    Improvements in FavouriteViewModel.kt(context error)

--- app/src/main/java/com/surajrathod/bcaprogram/ui/DescriptionActivity.kt
+++ app/src/main/java/com/surajrathod/bcaprogram/ui/DescriptionActivity.kt
@@ -10,8 +10,12 @@ import androidx.lifecycle.get
 import com.surajrathod.bcaprogram.R
 import com.surajrathod.bcaprogram.databinding.ActivityDescriptionBinding
 import com.surajrathod.bcaprogram.model.Program
+import com.surajrathod.bcaprogram.room.ProgramEntity
 import com.surajrathod.bcaprogram.viewmodel.FavouriteViewModel
 import kotlinx.android.synthetic.main.activity_description.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class DescriptionActivity : AppCompatActivity() {
 
@@ -32,9 +36,13 @@ class DescriptionActivity : AppCompatActivity() {
         val data = intent.getSerializableExtra("program") as Program
 
 
-        //favViewModel = ViewModelProvider(this@DescriptionActivity).get(FavouriteViewModel::class.java)
-
+        favViewModel = ViewModelProvider(this@DescriptionActivity).get(FavouriteViewModel()::class.java)
+        favViewModel.setUpDataBase(this)
         //favViewModel.favDb.programDao().isFav(data.id).toString()
+
+        GlobalScope.launch {
+            println(favViewModel.toggleFavourite(ProgramEntity(data.id,data.title,data.content,data.sem,data.sub,data.unit)))
+        }
 
 
         txtTitle.text = data.title.toString()
@@ -49,6 +57,7 @@ class DescriptionActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
     }
+
 
 
 
