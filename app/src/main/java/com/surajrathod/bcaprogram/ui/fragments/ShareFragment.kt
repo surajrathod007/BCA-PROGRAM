f639946ebcf25bfe81966f344edce4da71ad3146
Suraj <surajsinhrathod75@gmail.com>
2022-05-24 11:22:41 +0530

    ShareFragment.kt fixed

--- app/src/main/java/com/surajrathod/bcaprogram/ui/fragments/ShareFragment.kt
+++ app/src/main/java/com/surajrathod/bcaprogram/ui/fragments/ShareFragment.kt
@@ -1,6 +1,10 @@
 package com.surajrathod.bcaprogram.ui.fragments
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -25,13 +29,14 @@ class ShareFragment : Fragment() {
 
     //val update : AppUpdate = NetworkService.networkInstance.checkForUpdate()
 
-
     lateinit var btnShare : Button
     lateinit var app : Update
     lateinit var binding: FragmentShareBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        loadUpdate()
+
+        //
         super.onCreate(savedInstanceState)
     }
 
@@ -42,27 +47,38 @@ class ShareFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_share, container, false)
 
         binding = FragmentShareBinding.inflate(inflater,container,false)
+
+        var appLink = ""
+        if(checkInternet(requireContext())){
+            loadUpdate()
+            //appLink = app.link.toString()
+        }else{
+            binding.txtMessage.text = "Pls turn on internet"
+            appLink = " "
+        }
+
+
         btnShare = view.findViewById(R.id.btnShare)
 
 
 
 
 
-
         binding.btnShare.setOnClickListener {
-            val intent = Intent()
-            intent.action = Intent.ACTION_SEND
-            intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_TEXT,"Hey i found an app where you can get all BCA Practical programs for free!! \n Download Now : ${app.link}")
-            startActivity(Intent.createChooser(intent,"Share With Friends"))
+
+            if(checkInternet(it.context)){
+                val intent = Intent()
+                intent.action = Intent.ACTION_SEND
+                intent.type = "text/plain"
+                intent.putExtra(Intent.EXTRA_TEXT,"Hey i found an app where you can get all BCA Practical programs for free!! \n Download Now : ${app.link}")
+                startActivity(Intent.createChooser(intent,"Share With Friends"))
+            }else{
+                Toast.makeText(requireContext(),"No internet",Toast.LENGTH_SHORT).show()
+            }
+
+
 
         }
-
-
-
-
-
-
 
         return binding.root
 
@@ -106,6 +122,32 @@ class ShareFragment : Fragment() {
         })
 
 
+
+    }
+
+    fun checkInternet(context : Context) : Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when{
+
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+                else -> false
+
+            }
+        }else{
+
+            val netInfo = connectivityManager.activeNetworkInfo ?: return false
+
+            return netInfo.isConnected
+        }
 
     }
 
