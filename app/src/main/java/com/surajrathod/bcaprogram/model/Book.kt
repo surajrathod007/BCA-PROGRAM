c67934a6e8a3c75a43c4777d19b29069659a1aff
Suraj <surajsinhrathod75@gmail.com>
2023-03-26 17:57:10 +0530

    Books fetching done !

--- app/src/main/java/com/surajrathod/bcaprogram/model/Book.kt
+++ app/src/main/java/com/surajrathod/bcaprogram/model/Book.kt
@@ -1,12 +1,12 @@
 package com.surajrathod.bcaprogram.model
 
 data class Book(
-    val title : String,
-    val description : String,
-    val imageUrl : String,
-    val sem : String,
-    val sub : String,
-    val downloadLink : String,
+    val title : String="",
+    val description : String="",
+    val imageUrl : String="",
+    val sem : String="",
+    val sub : String="",
+    val downloadLink : String="",
     val demoImages : List<String>? = null,
     val tags : List<String>? = null
 )
